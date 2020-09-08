8e724f475a51ec2fda68529609856dc87b4f33a3
3Emme <emmettbuentiempo@gmail.com>
2020-09-08 10:42:15 -0700

    catch project up to date.
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,49 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './services/weather-service.js';
-import GiphyService from './services/giphy-service';
-
-function clearFields() {
-  $('#location').val("");
-  $('.show-errors').text("");
-  $('.show-gif').text("");
-}
-
-function displayWeatherDescription(description) {
-  $('.weather-description').text(`The weather is ${description}!`);
-}
-
-function displayGif(response) {
-  const url = response.data[0].images.downsized.url
-  $('.show-gif').html(`<img src='${url}'>`);
-}
-
-function displayErrors(error) {
-  $('.show-errors').text(`${error}`);
-}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    let city = $('#location').val();
-    clearFields();
-    WeatherService.getWeather(city)
-      .then(function(weatherResponse) {
-        if (weatherResponse instanceof Error) {
-          throw Error(`OpenWeather API error: ${weatherResponse.message}`);
-        }
-        const weatherDescription = weatherResponse.weather[0].description;
-        displayWeatherDescription(weatherDescription);
-        return GiphyService.getGif(weatherDescription);
-      })
-      .then(function(giphyResponse) {
-        if (giphyResponse instanceof Error) {
-          throw Error(`Giphy API error: ${giphyResponse.message}`);
-        }
-        displayGif(giphyResponse);
-      })
-      .catch(function(error) {
-        displayErrors(error.message)
-      })
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+   function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
   });
 });
